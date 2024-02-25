c89484c4a9fb057cdb8e1f45132f004c49063b55
Castellano46 <pedroliebana35@gmail.com>
2024-02-25 12:00:47 +0100

    Practica

---
+++ app/src/main/java/com/example/fundamentosandroid/heroes/HeroesActivity.kt
@@ -0,0 +1,74 @@
+package com.example.fundamentosandroid.heroes
+
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.fundamentosandroid.databinding.ActivityHeroesBinding
+import com.example.fundamentosandroid.heroes.herodetail.HeroDetailFragment
+import com.example.fundamentosandroid.heroes.heroeslist.Clicked
+import com.example.fundamentosandroid.heroes.heroeslist.ListHeroesFragment
+import kotlinx.coroutines.launch
+
+
+class HeroesActivity : AppCompatActivity(), Clicked {
+    private lateinit var binding: ActivityHeroesBinding
+    private val viewModel: HeroesViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHeroesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        lifecycleScope.launch{
+            viewModel.uiState.collect{
+                when (it){
+                    is HeroesViewModel.UiState.Idle ->  viewModel.downloadHeroes()
+                    is HeroesViewModel.UiState.OnHeroesReceived -> loadRecyclerView()
+                    is HeroesViewModel.UiState.OnHeroReceived -> onClick()
+                    is HeroesViewModel.UiState.OnHeroDamaged -> Unit
+                    is HeroesViewModel.UiState.OnHeroRecovered -> Unit
+                    is HeroesViewModel.UiState.Error -> showError(viewModel.uiState.value.toString())
+                }
+            }
+        }
+        loadRecyclerView()
+    }
+
+    override fun onClick(){
+        loadHeroDetail()
+    }
+
+    override fun goBack() {
+        loadRecyclerView()
+    }
+
+    private fun showError(error: String) {
+        Toast.makeText(this, "$error", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun loadRecyclerView() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.fFragment.id, ListHeroesFragment(this))
+            .commitNow()
+    }
+
+    private fun loadHeroDetail() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.fFragment.id, HeroDetailFragment(this))
+            .commitNow()
+    }
+    private fun getToken(): String {
+        var token:String
+        getPreferences(Context.MODE_PRIVATE).apply {
+            token = getString("Token", "").toString()
+            return token
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fundamentosandroid/login/LoginViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.fundamentosandroid.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import okhttp3.Credentials
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+class LoginViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow<UiState>(UiState.Idle)
+    val uiState : StateFlow<UiState> = _uiState
+    val BASE_URL = "https://dragonball.keepcoding.education/api/"
+    var token: String = ""
+    init {
+
+    }
+    fun login(email:String, password:String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val client = OkHttpClient()
+            val url = "${BASE_URL}auth/login"
+            val credentials = Credentials.basic("$email", "$password")
+            val formBody = FormBody.Builder()
+                .build()
+            val request = Request.Builder()
+                .url(url)
+                .addHeader("Authorization", "$credentials")
+                .post(formBody)
+                .build()
+            val call = client.newCall(request)
+            val response = call.execute()
+
+            if (response.code == 200){
+                response.body?.let { responseBody ->
+                    try {
+                        val token = responseBody.string()
+                        _uiState.value = UiState.OnTokenReceived(responseBody.toString())
+                    } catch(ex: Exception ) {
+                        _uiState.value = UiState.Error("Something went wrong in the response")
+                    }
+                } ?: run { _uiState.value = UiState.Error("Something went wrong in the request") }
+            }
+            else {
+                _uiState.value = UiState.Error("Error en usuario o contraseña")
+            }
+        }
+    }
+
+    sealed class UiState {
+        object Idle : UiState()
+        data class Error(val error: String) : UiState()
+        data class OnTokenReceived(val token:String) : UiState()
+    }
+}
